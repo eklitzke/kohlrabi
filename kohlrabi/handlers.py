57e4c4d2ade36246318d66690c59b70e19b08a00
Evan Klitzke <evan@yelp.com>
2010-11-08 10:24:05 -0800

    add pagination controls to home

--- kohlrabi/handlers.py
+++ kohlrabi/handlers.py
@@ -1,4 +1,5 @@
 import os
+import math
 from collections import defaultdict
 import datetime
 try:
@@ -48,16 +49,27 @@ class RequestHandler(tornado.web.RequestHandler):
 
 class Home(RequestHandler):
 
-    path = '/'
+    path = '/[0-9]*'
+
+    DATES_PER_PAGE = 14
 
     def get(self):
+        # this is pretty inefficient
         date_map = defaultdict(list)
         for tbl in db.report_tables:
             for d in tbl.dates():
                 date_map[d].append(tbl)
 
+        dates = sorted(date_map.keys(), reverse=True)
+        self.env['num_pages'] = int(math.ceil(len(dates) / float(self.DATES_PER_PAGE)))
+
+        # show DATES_PER_PAGE things per page
+        page = int(self.request.uri.split('/')[-1] or 1)
+        self.env['page'] = page
+        dates = dates[self.DATES_PER_PAGE * (page - 1) : self.DATES_PER_PAGE * page]
+
         self.env['reports'] = []
-        for k in sorted(date_map.keys(), reverse=True):
+        for k in dates:
             date = k.strftime('%Y-%m-%d')
             tables = sorted(date_map[k], key=lambda x: x.display_name)
             self.env['reports'].append((date, [{'name': table.display_name, 'table_name': table.__name__} for table in tables]))
